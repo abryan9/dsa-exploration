91915ee48dbce5d2385b0deef5d612838ac18de0
Alex Bryan <abryan9@uwyo.edu>
2024-11-15 10:28:05 -0700

    Added some comments.

--- held-karp/algo.py
+++ held-karp/algo.py
@@ -3,20 +3,20 @@ import math
 def tsp_hk(distanceMatrix):
     numCities = len(distanceMatrix)
     print(f"\n+ numCities: {numCities}")
-    if (numCities) <= 1:
+    if (numCities) <= 1: # Either there is nowhere at all or the only way we can move is to ourself.
         return 0
     
-    memo = {}
+    memo = {} # Python dictionary is basically just a map
 
     def find_tour_len(visitedCities, currentCity):
-        P2P = f"{visitedCities:08b}-{currentCity:08b}"
+        P2P = f"{visitedCities:08b}-{currentCity:08b}" # Expanded the keys to binary flag representation for clarity
         print(f"+ New entry: {P2P}")
 
         if (P2P in memo):
             print("  + Already calculated this.")
             return memo.get(P2P)
         
-        if (visitedCities == (1 << numCities) - 1):
+        if (visitedCities == (1 << numCities) - 1): # If all of the visitedCities bits are 1
             print("  | + All cities visited.")
             return distanceMatrix[currentCity][-1] | 0
         
